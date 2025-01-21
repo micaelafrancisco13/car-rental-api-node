f0949c99a6456f9710962802bc870d28c73d4ba8
sonicmaster99 <brianguy0831@gmail.com>
2025-01-21 09:00:43 +0800

    add frontend link to cors

--- startup/cors.js
+++ startup/cors.js
@@ -3,7 +3,7 @@ const cors = require("cors")
 module.exports = function (app) {
 	app.use(
 		cors({
-			origin: [`http://localhost:${process.env.UI_CLIENT_PORT}`],
+			origin: [`http://localhost:${process.env.UI_CLIENT_PORT}`, 'https://car-rental-ui-react.vercel.app'],
 		}),
 	)
 }

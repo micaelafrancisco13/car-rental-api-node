6944c4b867292dd8adafdf99530e9f579df4ef78
sonicmaster99 <brianguy0831@gmail.com>
2025-03-16 18:42:14 +0800

    allow agreed

--- models/booking.js
+++ models/booking.js
@@ -7,7 +7,8 @@ function getJoiSchema() {
 		endLocation: joi.string().required().label("Ending Location"),
 		startDate: joi.date().iso().required().label("Starting Date"),
 		endDate: joi.date().iso().min(joi.ref("startDate")).required().label("Ending Date"),
-		paymentMode: joi.string().required().label("Payment Mode")
+		paymentMode: joi.string().required().label("Payment Mode"),
+		agreed: joi.bool().required().label("Agreed")
 	})
 }
 
